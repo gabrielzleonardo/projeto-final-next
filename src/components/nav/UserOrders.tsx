dfaa22342e27ceb91e401ac49cce935fb14eff3a
gabriel <gleonardoalano@gmail.com>
2023-07-10 18:34:41 -0300

    fix: userorders button usuario comum

--- src/components/nav/UserOrders.tsx
+++ src/components/nav/UserOrders.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 const UserOrders = () => {
+  const numberOfOrders = 0;
   return (
-    <Link className="relative" href="/">
+    <Link className="relative lg:btn-primary flex items-center gap-2" href="/">
       <div className="bg-tomato-100 h-6 w-6 rounded-full absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 text-center lg:hidden">
         <span>0</span>
       </div>
@@ -12,6 +13,8 @@ const UserOrders = () => {
         height={22}
         alt="Meus pedidos"
       />
+
+      <span className="hidden lg:block">Pedidos ({numberOfOrders})</span>
     </Link>
   );
 };
