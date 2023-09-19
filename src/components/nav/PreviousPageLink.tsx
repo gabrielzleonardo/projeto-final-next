a8427d26cd961b51170dee5f7b5f76bd3facb9bc
gabriel <gleonardoalano@gmail.com>
2023-09-19 17:24:50 -0300

    feat: previous page link component

--- src/components/nav/PreviousPageLink.tsx
+++ src/components/nav/PreviousPageLink.tsx
@@ -1,8 +1,27 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const PreviousPageLink = (href: string, customClassName: string) => {
-  return <Link href={href}>Voltar</Link>;
+interface IPreviousPageLink {
+  href: string;
+  customClassName?: string;
+}
+
+const PreviousPageLink = ({ href, customClassName }: IPreviousPageLink) => {
+  return (
+    <Link
+      href={href}
+      className={`${customClassName} text-link text-base flex items-center w-fit gap-2 text-light-300 lg:text-2xl lg:font-bold`}
+    >
+      <Image
+        src="/ui-icons/leftArrow-icon.svg"
+        width={12}
+        height={22}
+        alt="return icon"
+        className=""
+      />
+      <span>Voltar</span>
+    </Link>
+  );
 };
 
 export default PreviousPageLink;
