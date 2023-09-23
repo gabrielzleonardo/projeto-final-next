d343a502a66acf090bce4c59caa3589de4b9484c
gabriel <gleonardoalano@gmail.com>
2023-09-23 19:07:54 -0300

    fix: import type

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import  prisma  from "@/lib/prisma";
 
 export async function GET() {
   const categories = await prisma.category.findMany();
